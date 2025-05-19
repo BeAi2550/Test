ca61424e2580cf0ddd1978a2b1caebcd46265b66
BeAi2550 <progamerdv8@gmail.com>
2025-05-20 01:58:39 +0700

    Sakura stand rahhh-1.lua

--- Sakura stand rahhh-1.lua
+++ Sakura stand rahhh-1.lua
@@ -3258,8 +3258,8 @@ local HomeTab = DrRayLibrary.newTab("Home","http://www.roblox.com/asset/?id=9405
 
 -- // Apply Custom UI Color \\ --
 game:GetService("CoreGui").DrRay.TopBar.BackgroundColor3 = Color3.new(25,5,35);
-game:GetService("CoreGui").DrRay.MainBar.BackgroundColor3 = Color3.new(180,130,255);
-game:GetService("CoreGui").DrRay.TopBar.TopBar.BackgroundColor3 = Color3.new(150,80,200);
+game:GetService("CoreGui").DrRay.MainBar.BackgroundColor3 = Color3.new(70,40,90);
+game:GetService("CoreGui").DrRay.TopBar.TopBar.BackgroundColor3 = Color3.new(0,0,0);
 game:GetService("CoreGui").DrRay.TopBar.ProfileMenu.Clock.BackgroundTransparency = 0.75;
 game:GetService("CoreGui").DrRay.TopBar.ProfileMenu.PlayerProfile.BackgroundTransparency = 0.75;
 game:GetService("CoreGui").DrRay.TopBar.ProfileMenu.Title.BackgroundTransparency = 0.75;
